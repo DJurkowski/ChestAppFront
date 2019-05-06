be41d9101588b4eac7e6742a65141684eee89468
Dominik Jurkowski <jurkowski.dominik.andrzej@gmail.com>
2019-05-06 14:47:10 +0200

    Notification modyfication added, plus new display

--- src/app/notifications/notifications/notifications.component.ts
+++ src/app/notifications/notifications/notifications.component.ts
@@ -2,7 +2,8 @@ import { TokenStorageService } from './../../auth/token-storage.service';
 import { NotificationService } from './../../services/notification.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Notification } from './notification';
+import { Notification} from './../../notifications/notifications/notification';
+
 
 @Component({
   selector: 'app-notifications',
@@ -12,6 +13,7 @@ import { Notification } from './notification';
 export class NotificationsComponent implements OnInit {
 
   notificationList: Observable<Notification[]>;
+  notiList: Array<Notification> = new Array<Notification>();
   username: string;
 
   constructor(private notificationService: NotificationService, private token: TokenStorageService) { }
@@ -22,7 +24,40 @@ export class NotificationsComponent implements OnInit {
   }
 
   reloadData() {
+    this.notiList = [];
     this.notificationList = this.notificationService.getNotifications(this.username);
+
+    this.notificationList.forEach(data => {
+      data.forEach( xdata => {
+        this.notiList.push ( xdata );
+      });
+    });
+  }
+
+  seenFunction(id: number) {
+    for (const i of this.notiList) {
+      if (i.id === id) {
+        i.seen = !i.seen;
+        this.notificationService.modifyNotification(this.username, i.id, i).subscribe(
+          data => {
+            this.reloadData();
+          }
+        );
+      }
+    }
+  }
+
+  deleteNotification(id: number) {
+    for (const i of this.notiList) {
+      if ( i.id === id) {
+        console.log('Jestem !!');
+        this.notificationService.deleteNotification(id, this.username).subscribe(
+          data => {
+            this.reloadData();
+          }
+          );
+       }
+    }
   }
 
 }
--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { Notification } from './../../app/notifications/notifications/notification';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,4 +15,12 @@ export class NotificationService {
     return this.http.get(`${this.baseUrl}/${userId}` + `/notifications`);
   }
 
+  modifyNotification(userId: string, notiId: number, notification: Notification): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${userId}` + `/notification` + `/${notiId}`, notification);
+  }
+
+  deleteNotification(notiId: number, userId: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${userId}` + `/notification` + `/${notiId}`, { responseType: 'text'});
+
+  }
 }
